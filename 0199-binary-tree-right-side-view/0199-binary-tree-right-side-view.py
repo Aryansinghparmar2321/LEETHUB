5ac6086f4b218cc87261f2d4dc29731c8224df6e
Aryansinghparmar2321 <aryanparmar2321@gmail.com>
2026-04-22 01:50:51 +0530

    Time: 3 ms (8.9%), Space: 19.4 MB (12.33%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        result = []
+        queue = deque([root])
+        
+        while queue:
+            size = len(queue)
+            
+            for i in range(size):
+                node = queue.popleft()
+                
+                if i == size - 1:
+                    result.append(node.val)
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        
+        return result
